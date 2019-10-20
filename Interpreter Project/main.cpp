4fcc860cf1b70c733b7286d552450b687dea1ece
ChayaphonPugkhem <phoncp@gmail.com>
2019-10-19 22:22:02 -0400

    'jdfjd'

--- Interpreter Project/main.cpp
+++ Interpreter Project/main.cpp
@@ -3,6 +3,29 @@
 using namespace std;
 
 int main(void) {
+
+	char * memblock;
+
+	streampos size;
+	ifstream fpin("interpreter_input.smp", ios::in | ios::binary);
+//	fpin.open("interpreter_intput.smp", ios::in | ios::binary);
+
+
+	if (fpin.is_open()) {
+		size = fpin.tellg();
+		cout << fpin.tellg <<endl;
+		memblock = new char [size];
+		fpin.seekg(0, ios::beg);
+		fpin.read(memblock, size);
+		fpin.close();
+		cout << "the entire file content is in the memory" << endl;
+
+
+
+		delete[] memblock;
+	}
+	else cout << "Unable to open file";
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "John" << endl; // prints !!!Hello World!!!
 
