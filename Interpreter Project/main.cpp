599e67b5edca894a9b016f33dfcd28492711996e
Ben Sukboontip <ben.sukboontip@gmail.com>
2019-10-19 22:17:01 -0400

    input reading

--- Interpreter Project/main.cpp
+++ Interpreter Project/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include<stdlib.h>
 using namespace std;
 
 int main(void) {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "John" << endl; // prints !!!Hello World!!!
+	
+
+	FILE *inputF = fopen("interpreter_input.smp", "rb");
+	fseek(inputF, 0, SEEK_END);
+	long inputSize = ftell(inputF);
+	rewind(inputF);
+
+	char* buffer = (char*) malloc(sizeof(char) * inputSize);
+	if (buffer == NULL) {
+		fputs("memory error", stderr);
+		exit(1);
+	}
+
+	long result = fread(buffer, 1, inputSize, inputF);
+	if (result != inputSize) {
+		fputs("reading error", stderr);
+		exit(2);
+	}
+
+	cout << result << endl;
+	for (int i = 0; i < inputSize; i++){
+		cout << buffer[i] << endl;
+	}
+
+	fclose(inputF);
+	free(buffer);
 
 	return 0;
 }
