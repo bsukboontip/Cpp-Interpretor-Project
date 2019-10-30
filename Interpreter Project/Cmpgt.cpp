bd9a0c241805512b329cc2b87a83040921f101f0
ChayaphonPugkhem <phoncp@gmail.com>
2019-10-30 00:45:23 -0400

    comparison

--- Interpreter Project/Cmpgt.cpp
+++ Interpreter Project/Cmpgt.cpp
@@ -5,5 +5,26 @@ Cmpgt::Cmpgt(){}
 Cmpgt::~Cmpgt(){}
 
 int Cmpgt::execute(vector<StackValues*> rstack, stack<int>fpstack, int sp, int fpsp, int pc){
-	return 0;
+	if (rstack[sp]->type == 'c' && rstack[sp-1]->type == 'c') {
+		rstack[sp-1]->c = rstack[sp-1]->c > rstack[sp]->c;
+		rstack.pop_back();	
+		sp--;
+	}
+	else if (rstack[sp]->type == 's' && rstack[sp-1]->type == 's') {
+		rstack[sp-1]->s = rstack[sp-1]->s > rstack[sp]->s;
+		rstack.pop_back();	
+		sp--;		
+	}
+	else if (rstack[sp]->type == 'i' && rstack[sp-1]->type == 'i') {
+		rstack[sp-1]->i = rstack[sp-1]->i > rstack[sp]->i;
+		rstack.pop_back();	
+		sp--;
+	}
+	else if (rstack[sp]->type == 'f' && rstack[sp-1]->type == 'f') {
+		rstack[sp-1]->f = rstack[sp-1]->f > rstack[sp]->f;
+		rstack.pop_back();	
+		sp--;		
+	}
+	pc++;
+	return pc;
 }
--- Interpreter Project/Prints.cpp
+++ Interpreter Project/Prints.cpp
@@ -6,6 +6,7 @@ Prints::~Prints(){}
 
 int Prints::execute(vector<StackValues*> rstack, stack<int>fpstack, int sp, int fpsp, int pc){
 	cout << rstack[sp--]->s << endl;
+	rstack.pop_back();	
 	pc++;
 	return pc;
 }
