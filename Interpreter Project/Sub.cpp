b41b6fe43ee5ccc2c416daedcde554342e4c73c9
ChayaphonPugkhem <phoncp@gmail.com>
2019-10-30 00:05:05 -0400

    aritmetic done

--- Interpreter Project/Sub.cpp
+++ Interpreter Project/Sub.cpp
@@ -3,21 +3,21 @@
 Sub::Sub(){}
 Sub::~Sub(){}
 
-int Sub::execute(vector<StackValues*>, stack<int>, int, int, int){
+int Sub::execute(vector<StackValues*> rstack, stack<int>fpstack, int sp, int fpsp, int pc){
 	
-	if (rstack[sp]->type == 'c' && rstack[sp-1] == 'c') {
+	if (rstack[sp]->type == 'c' && rstack[sp-1]->type == 'c') {
 		rstack[sp-1]->c = rstack[sp-1]->c + rstack[sp]->c;
 		sp--;
 	}
-	else if (rstack[sp]->type == 's' && rstack[sp-1] == 's') {
+	else if (rstack[sp]->type == 's' && rstack[sp-1]->type == 's') {
 		rstack[sp-1]->s = rstack[sp-1]->s - rstack[sp]->s;
 		sp--;		
 	}
-	else if (rstack[sp]->type == 'i' && rstack[sp-1] == 'i') {
+	else if (rstack[sp]->type == 'i' && rstack[sp-1]->type == 'i') {
 		rstack[sp-1]->i = rstack[sp-1]->i - rstack[sp]->i;
 		sp--;
 	}
-	else if (rstack[sp]->type == 'f' && rstack[sp-1] == 'f') {
+	else if (rstack[sp]->type == 'f' && rstack[sp-1]->type == 'f') {
 		rstack[sp-1]->f = rstack[sp-1]->f - rstack[sp]->f;
 		sp--;		
 	}
