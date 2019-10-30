98ae00b88cc736ec56e7619d96212dc565aadb5d
Ben Sukboontip <ben.sukboontip@gmail.com>
2019-10-30 00:09:42 -0400

    for pulling

--- Interpreter Project/Add.cpp
+++ Interpreter Project/Add.cpp
@@ -6,24 +6,24 @@ Add::~Add(){}
 
 int Add::execute(vector<StackValues*> rstack, stack<int>fpstack, int sp, int fpsp, int pc){
 
-	if (rstack[sp]->type == 'c' && rstack[sp-1] == 'c') {
-		rstack[sp-1]->c = rstack[sp-1]->t + rstack[sp]->t;
-		sp--;
-	}
-	else if (rstack[sp]->type == 's' && rstack[sp-1] == 's') {
-		rstack[sp-1]->s = rstack[sp-1]->s + rstack[sp]->s;
-		sp--;		
-	}
-	else if (rstack[sp]->type == 'i' && rstack[sp-1] == 'i') {
-		rstack[sp-1]->i = rstack[sp-1]->i + rstack[sp]->i;
-		sp--;
+	// if (rstack[sp]->type == 'c' && rstack[sp-1] == 'c') {
+	// 	rstack[sp-1]->c = rstack[sp-1]->t + rstack[sp]->t;
+	// 	sp--;
+	// }
+	// else if (rstack[sp]->type == 's' && rstack[sp-1] == 's') {
+	// 	rstack[sp-1]->s = rstack[sp-1]->s + rstack[sp]->s;
+	// 	sp--;		
+	// }
+	// else if (rstack[sp]->type == 'i' && rstack[sp-1] == 'i') {
+	// 	rstack[sp-1]->i = rstack[sp-1]->i + rstack[sp]->i;
+	// 	sp--;
 
 		
-	}
-	else if (rstack[sp]->type == 'f' && rstack[sp-1] == 'f') {
-		rstack[sp-1]->f = rstack[sp-1]->t + rstack[sp]->f;
-		sp--;		
-	}
-	pc++;
+	// }
+	// else if (rstack[sp]->type == 'f' && rstack[sp-1] == 'f') {
+	// 	rstack[sp-1]->f = rstack[sp-1]->t + rstack[sp]->f;
+	// 	sp--;		
+	// }
+	// pc++;
 	return pc;
 }
--- Interpreter Project/main.cpp
+++ Interpreter Project/main.cpp
@@ -50,6 +50,7 @@
 #include "Pokei.h"
 #include "Pokes.h"
 #include "Swp.h"
+#include "Halt.h"
 #include "StackValues.h"
 
 using namespace std;
@@ -83,6 +84,7 @@ int main(void) {
 	int pc = 0;
 	int i;
 	MemoryObject* theOne[inputSize];
+	// cout << inputSize << endl;
 	MemoryObject* newOne;
 
 	for (i = 0; i < inputSize; i++) {
@@ -267,42 +269,52 @@ int main(void) {
 		else if (memory[i] == 147) {
 			theOne[i] = new Printf();
 		}
+
+		else if (memory[i] == 0) {
+			newOne = new Halt();
+			theOne[i] = newOne;
+			// theOne[i] = new Halt();
+			cout << "halted " << i << endl;
+		}
 	}
 
 	StackValues* newStack;
 	pc = 0;
 	
-	while(pc != -1) {
-		if (pc == 68) {
-			newStack = new StackValues(theOne[pc + 1]->getChar());
-			rstack.push_back(newStack);
-			pc += 2;
-		}
-		else if (pc == 69) {
-			newStack = new StackValues(theOne[pc + 1]->getShort());
-			rstack.push_back(newStack);
-			pc += 3;
-		}
-		else if (pc == 70) {
-			newStack = new StackValues(theOne[pc + 1]->getInt());
-			rstack.push_back(newStack);
-			pc += 5;
-		}
-		else if (pc == 71) {
-			newStack = new StackValues(theOne[pc + 1]->getFloat());
-			rstack.push_back(newStack);
-			pc += 5;
-		}
-		else {
-			pc = theOne[pc]->execute(rstack, fpstack, sp, fpsp, pc);
-		}
-	}
-	// test[0] = new Add();
-	// sp
-	// pc = test[0]->add(rstack)
+	// while(pc != -1) {
+	// 	if (pc == 68) {
+	// 		newStack = new StackValues(theOne[pc + 1]->getChar());
+	// 		rstack.push_back(newStack);
+	// 		sp += 1;
+	// 		pc += 2;
+	// 	}
+	// 	else if (pc == 69) {
+	// 		newStack = new StackValues(theOne[pc + 1]->getShort());
+	// 		rstack.push_back(newStack);
+	// 		sp += 1;
+	// 		pc += 3;
+	// 	}
+	// 	else if (pc == 70) {
+	// 		newStack = new StackValues(theOne[pc + 1]->getInt());
+	// 		rstack.push_back(newStack);
+	// 		pc += 5;
+	// 		sp += 1;
+	// 	}
+	// 	else if (pc == 71) {
+	// 		newStack = new StackValues(theOne[pc + 1]->getFloat());
+	// 		rstack.push_back(newStack);
+	// 		pc += 5;
+	// 		sp += 1;
+	// 	}
+	// 	else {
+	// 		// pc++;
+	// 		pc = theOne[pc]->execute(rstack, fpstack, sp, fpsp, pc);
 
+	// 	}
+	// }
 
-
+	cout << rstack.size() << endl;
+	cout << "HELLO" << endl;
 
 	fclose(inputF);
 	free(memory);
